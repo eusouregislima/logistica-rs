d9508028e54e51de26be39c940d7f63fe18c658d
Rodolfo Mori <rodolfomori@Rodolfos-MacBook-Pro.local>
2024-05-09 22:55:03 -0300

    Ultima versao estável

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,73 +1,64 @@
-import React, { useState, useEffect } from "react";
-import { useKeepAwake } from "expo-keep-awake";
-import { Button, Container, Paragraph, CoordinatesText, Input, ParagraphSpan, ParagraphTitle } from "./styles";
-import { getCurrentLocationAsync } from '../../services/LocationService';
-import axios from 'axios'
+import React, { useState } from 'react'
+import { useKeepAwake } from 'expo-keep-awake'
+import {
+  Button,
+  Container,
+  Paragraph,
+  CoordinatesText,
+  Input,
+  ParagraphSpan,
+  ParagraphTitle,
+} from './styles'
+import { getCurrentLocationAsync } from '../../services/LocationService'
+import api from '../../services/api'
 
 function Home() {
-  useKeepAwake();
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [coordinates, setCoordinates] = useState(null);
-  const [intervalId, setIntervalId] = useState(null);
-
-  const url = `https://logistica-rs.com.br/api-tracking-rs/received-webhook-location?lat=${coordinates?.latitude}&longitude=${coordinates?.longitude}&u=${phoneNumber}`;
-  console.log(url)
+  useKeepAwake()
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [intervalId, setIntervalId] = useState(null)
+  const [isLocation, setIsLocation] = useState(false);
 
   const getLocation = async () => {
     try {
-      const coords = await getCurrentLocationAsync();
-      console.log('Localização atual:', coords);
-      setCoordinates(coords);
+      const coordData = await getCurrentLocationAsync()
+      console.log('Localização atual:', coordData)
+      sendLocation(coordData)
     } catch (error) {
-      console.error('Erro ao obter localização:', error);
+      console.error('Erro ao obter localização:', error)
     }
-    finally {
-      sendLocation()
-    }
-  };
-
-  const sendLocation = async () => {
-    try {
-      await axios.get(url);
-    }
-    catch (error) {
-      console.error(error)
-    }
-
   }
 
   const startInterval = () => {
     if (!intervalId) {
       const id = setInterval(() => {
-        getLocation();
-      }, 60000);
-      setIntervalId(id);
+        getLocation()
+      }, 60000)
+      setIntervalId(id)
     }
-  };
+  }
 
-  const stopInterval = () => {
-    clearInterval(intervalId);
-    setIntervalId(null);
 
-  };
+  const sendLocation = async (coordinates) => {
+    try {
+      const data = await api.get(
+        `api-tracking-rs/received-webhook-location?lat=37.4220936&longitude=-122.083922&u=11968290266`,
+      )
 
-  useEffect(() => {
-    if (phoneNumber) {
-      startInterval();
-    } else {
-      stopInterval();
+      console.log(data.status)
+      startInterval()
+      setIsLocation(true)
+    } catch (error) {
+      console.error(error)
     }
-  }, [phoneNumber]);
-
+  }
 
   const handleLocationButtonClick = async () => {
-    if (phoneNumber !== "") {
-      getLocation();
-    }
-    else {
+    if (phoneNumber !== '') {
+      getLocation()
+    } else {
       return
     }
-  };
+  }
 
   return (
     <Container>
@@ -83,14 +74,13 @@ function Home() {
       <Button onPress={handleLocationButtonClick}>
         <Paragraph>Envie sua localização</Paragraph>
       </Button>
-      {coordinates && (
+      {isLocation && (
         <Container>
           <Paragraph>Sua localização está sendo enviada</Paragraph>
         </Container>
       )}
-
     </Container>
   )
 }
 
-export default Home;
+export default Home
