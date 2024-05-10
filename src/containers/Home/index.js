fc52b43b45fbd52bb9f7b87139a75561cb3a2de8
Reginaldo Lima <108771153+eusouregislima@users.noreply.github.com>
2024-05-09 21:40:58 -0300

    commit para teste

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,29 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { useKeepAwake } from "expo-keep-awake";
-import { Button, Container, Paragraph, CoordinatesText, Input, ParagraphSpan, ParagraphTitle, ButtonStop } from "./styles";
+import { Button, Container, Paragraph, CoordinatesText, Input, ParagraphSpan, ParagraphTitle } from "./styles";
 import { getCurrentLocationAsync } from '../../services/LocationService';
 import axios from 'axios'
 
 function Home() {
   useKeepAwake();
-  const [name, setName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [coordinates, setCoordinates] = useState(null);
   const [intervalId, setIntervalId] = useState(null);
 
-  const url = `https://logistica-rs.com.br/api-tracking-rs/received-webhook-location?lat=${coordinates?.latitude}&long=${coordinates?.longitude}&u=${phoneNumber}&name=${name}`;
+  const url = `https://logistica-rs.com.br/api-tracking-rs/received-webhook-location?lat=${coordinates?.latitude}&long=${coordinates?.longitude}&u=${phoneNumber}`;
+  console.log(url)
 
   const getLocation = async () => {
     try {
       const coords = await getCurrentLocationAsync();
       console.log('Localização atual:', coords);
       setCoordinates(coords);
-
-      await axios.get(url);
     } catch (error) {
       console.error('Erro ao obter localização:', error);
     }
+    finally {
+      sendLocation()
+    }
   };
+
+  const sendLocation = async () => {
+    try {
+      await axios.get(url);
+    }
+    catch (error) {
+      console.error(error)
+    }
+
+  }
 
   const startInterval = () => {
     if (!intervalId) {
@@ -41,16 +52,16 @@ function Home() {
   };
 
   useEffect(() => {
-    if (name && phoneNumber) {
+    if (phoneNumber) {
       startInterval();
     } else {
       stopInterval();
     }
-  }, [name, phoneNumber, intervalId]);
+  }, [phoneNumber]);
 
 
   const handleLocationButtonClick = async () => {
-    if (name !== "" && phoneNumber !== "") {
+    if (phoneNumber !== "") {
       getLocation();
     }
     else {
@@ -61,12 +72,7 @@ function Home() {
   return (
     <Container>
       <ParagraphTitle>LOGÍSTICA RS</ParagraphTitle>
-      <ParagraphSpan>Informe seu Nome Completo</ParagraphSpan>
-      <Input
-        placeholder="Digite o seu Nome"
-        value={name}
-        onChangeText={(text) => setName(text)}
-      />
+
       <ParagraphSpan>Informe seu telefone</ParagraphSpan>
       <Input
         placeholder="Digite o seu Telefone"
@@ -74,18 +80,12 @@ function Home() {
         onChangeText={(text) => setPhoneNumber(text)}
       />
 
-
       <Button onPress={handleLocationButtonClick}>
         <Paragraph>Envie sua localização</Paragraph>
       </Button>
       {coordinates && (
         <Container>
           <Paragraph>Sua localização está sendo enviada</Paragraph>
-          <Paragraph>Nome: {name}</Paragraph>
-          <Paragraph>Número de telefone: {phoneNumber}</Paragraph>
-          <Paragraph>Latitude: {coordinates.latitude}, Longitude: {coordinates.longitude}</Paragraph>
-          <Paragraph>URL {url}</Paragraph>
-
         </Container>
       )}
 
