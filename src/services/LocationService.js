5ac32ee65b4d637323b12c59a6daef1e8f4cf664
Rodolfo Mori <rodolfomori@Rodolfos-MacBook-Pro.local>
2024-05-10 22:08:36 -0300

    Ultima versao estável

--- src/services/LocationService.js
+++ src/services/LocationService.js
@@ -1,6 +1,22 @@
 import * as Location from 'expo-location';
 
 async function getCurrentLocationAsync() {
+  await Location.startLocationUpdatesAsync('Missão RS', {
+    accuracy: Location.Accuracy.Highest,
+    timeInterval: 1000,
+    distanceInterval: 0,
+    showsBackgroundLocationIndicator: true,
+    foregroundService: {
+        notificationTitle: "Location Tracking",
+        notificationBody: "Tracking your location for routing purposes",
+        notificationColor: "#FF0000",
+    },
+    pausesUpdatesAutomatically: false,
+    deferredUpdatesInterval: 1000,
+    deferredUpdatesDistance: 0,
+    deferredUpdatesTimeout: 1000,
+});
+
   let { status } = await Location.requestForegroundPermissionsAsync();
   if (status !== 'granted') {
     throw new Error('Permissão de localização não concedida');
@@ -11,3 +27,4 @@ async function getCurrentLocationAsync() {
 }
 
 export { getCurrentLocationAsync };
+
